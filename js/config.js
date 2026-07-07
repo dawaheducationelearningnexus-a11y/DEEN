788e7fbb6e0cf35a1327d7ecfd0a84e497604f45
DEEN <dawaheducationelearningnexus@gmail.com>
2026-07-07 11:45:21 +0600

    Add global configuration for DEEN website

--- js/config.js
+++ js/config.js
@@ -1,1 +1,71 @@
+"use strict";
 
+/* ==========================================================
+   DEEN WEBSITE
+   GLOBAL CONFIGURATION
+   Version : 1.0.0
+========================================================== */
+
+window.DEEN = window.DEEN || {};
+
+DEEN.config = {
+
+    /* ---------- Website ---------- */
+
+    siteName: "DEEN",
+
+    fullName: "Dawah Education & E-learning Nexus",
+
+    slogan: "Knowledge Changes Lives",
+
+    version: "1.0.0",
+
+    author: "DEEN",
+
+    year: 2026,
+
+    /* ---------- Features ---------- */
+
+    enableLoader: true,
+
+    enableAnimations: true,
+
+    enableCounter: true,
+
+    enableTheme: true,
+
+    enableLanguage: true,
+
+    enableScrollTop: true,
+
+    enableSmoothScroll: true,
+
+    enableReveal: true,
+
+    enableRipple: true,
+
+    enableNavbarShadow: true,
+
+    /* ---------- Animation ---------- */
+
+    animationDuration: 600,
+
+    animationDelay: 120,
+
+    counterDuration: 2000,
+
+    /* ---------- Responsive ---------- */
+
+    mobileWidth: 992,
+
+    tabletWidth: 1200,
+
+    /* ---------- Scroll ---------- */
+
+    scrollOffset: 80,
+
+    scrollTopOffset: 500
+
+};
+
+Object.freeze(DEEN.config);
